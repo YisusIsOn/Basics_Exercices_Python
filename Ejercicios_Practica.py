7c69c65441958d3c3384ea760d7dcb0589aba033
Jesus David <jsusilvao01@gmail.com>
2023-06-15 18:29:54 -0500

    Se agrego el ejercicio 21 a Yisus

--- Ejercicios_Practica.py
+++ Ejercicios_Practica.py
@@ -1,12 +1,6 @@
-"Ejercicio 21"
+"Ejercicio 22"
 
 """
-Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa 
-y muestra por pantalla, el día, el mes y el año. Adaptar el programa anterior para que 
-también funcione cuando el día o el mes se introduzcan con un solo carácter.
+Escribir un programa que pregunte por consola por los productos de una cesta de la compra, 
+separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
 """
-
-
-
-
-
---
+++ Ejercicios_YisusOn/Ejercicio_21_MencionarFormatoFecha.py
@@ -0,0 +1,34 @@
+"Ejercicio 21"
+
+"""
+Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa 
+y muestra por pantalla, el día, el mes y el año. Adaptar el programa anterior para que 
+también funcione cuando el día o el mes se introduzcan con un solo carácter.
+"""
+
+def ingreso_Datos():
+    print("\n")
+    print("FECHA DE NACIMIENTO".center(70))
+    print("".center(70, "-"))
+    date_OfBorn = input("Ingrese su fecha de nacimiento en el formato dd/mm/aaa:  ")
+    print("".center(70, "-"))
+    return date_OfBorn
+
+def div_Format(date_OfBorn):
+    date_OfBornVector =[] 
+    day = date_OfBorn.split("/")[0]
+    month = date_OfBorn.split("/")[1]
+    age = date_OfBorn.split("/")[2]
+    return day, month, age
+
+def user_Out(day, month, age):
+    print("El dia es: {}".format(day).center(70))
+    print("El mes es: {}".format(month).center(70))
+    print("El año es: {}".format(age).center(70))
+    print("".center(70, "-"))
+    print("\n")
+
+if __name__ == "__main__":
+    date_OfBorn = ingreso_Datos()
+    day, month, age = div_Format(date_OfBorn)
+    user_Out(day, month, age)
