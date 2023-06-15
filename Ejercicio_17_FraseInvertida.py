3783d6a6b043117a4efab57774f6449a4905a6a7
Jesus David <jsusilvao01@gmail.com>
2023-06-14 23:04:01 -0500

    Se agrego el ejercicio 17

--- Ejercicio_17_FraseInvertida.py
+++ Ejercicio_17_FraseInvertida.py
@@ -6,3 +6,10 @@ y después muestre por pantalla la misma frase pero con la vocal introducida en 
 """
 "Modo Basico"
 
+print("\n")
+print("".center(50,"-"))
+frase =input("Ingrese una frase: ")
+frase_Invertida = frase[::-1]
+print("La frase invertida es: " + frase_Invertida)
+print("".center(50,"-"))
+print("\n")
--- Ejercicios_Practica.py
+++ Ejercicios_Practica.py
@@ -1,7 +1,7 @@
 "Ejercicio 18"
 
 """
-Escribir un programa que pregunte por consola el precio de un producto en euros con dos decimales 
-y muestre por pantalla el número de euros y el número de céntimos del precio introducido.
+Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
 """
 "Modo Basico"
+
