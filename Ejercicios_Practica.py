a87ef195a890af522c92cb276223696dda87437f
Jesus David <jsusilvao01@gmail.com>
2023-06-20 18:02:28 -0500

    Se agrega el ejercicio 24

--- Ejercicios_Practica.py
+++ Ejercicios_Practica.py
@@ -1,6 +1,8 @@
-"Ejercicio 24"
+"Ejercicio 25"
 
 """
-Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
+pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida 
+por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
 """
 
---
+++ Ejercicios_YisusOn/Ejercicio_24_EdadMayor.py
@@ -0,0 +1,17 @@
+"Ejercicio 24"
+
+"""
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+"""
+
+print("\n")
+print("".center(50,"-"))
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
+
+print("".center(50,"-"))
+print("\n")
